0967dc9024880a480f8fd54afd3a4a7ea5b810e2
Anish Sebastian <anishst@hotmail.com>
2019-09-27 09:16:05 -0400

    updates

--- Python/functions_practice/get_all_functions.py
+++ Python/functions_practice/get_all_functions.py
@@ -2,18 +2,20 @@ from os import listdir,chdir
 import inspect
 
 
-default_path = r'C:\Users\532975\Documents\Automation\SeleniumPythonFramework\tests\Stability'
-modules = [ fl for fl in listdir(default_path) if fl.endswith('.py') ]
+# default_path = r'C:\Users\532975\Documents\Automation\SeleniumPythonFramework\tests\Stability'
+# modules = [ fl for fl in listdir(default_path) if fl.endswith('.py') ]
 
 
 
 class Test(object):
 	"""docstring for Test"""
-	def test_method():
+	def test_method(self):
 		"""Test method doc"""
+		pass
 
-	def test_method2():
+	def test_method2(self):
 		"""Test method 2"""
+		pass
 
 print(inspect.getdoc(Test))
 print(inspect.getsource(Test))
