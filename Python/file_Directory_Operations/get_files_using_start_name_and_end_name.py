3b2f4aa3b993d89abacd84799e0820c208c8c98c
sebastian-anish <sebastian_anish@bah.com>
2020-03-25 10:24:40 -0400

    python updates

--- Python/file_Directory_Operations/get_files_using_start_name_and_end_name.py
+++ Python/file_Directory_Operations/get_files_using_start_name_and_end_name.py
@@ -14,5 +14,27 @@ def verify_jaser_report_file_download(start_value, end_value):
         print(f"Error handle file check: {e}")
 
 
+def get_only_latest_file(start_value, end_value):
+    download_location = os.environ['USERPROFILE'] + '/Downloads'
+    files_found = [filename for filename in os.listdir(download_location) if
+                   filename.startswith(start_value) and filename.endswith(end_value)]
+    print(files_found)
+    for name in sorted(files_found, key=lambda name: os.path.getmtime(os.path.join(download_location, name))):
+        print(name)
+    paths = [os.path.join(download_location, basename) for basename in files_found]
+    latest_file = max(paths, key=os.path.getmtime)
+    print(latest_file)
 
-verify_jaser_report_file_download('AgencyCIRA', 'pdf')
+def using_glob(start_value, end_value):
+    import glob
+    import os
+    download_location = os.environ['USERPROFILE'] + '/Downloads'
+    list_of_files = glob.glob(download_location + '/' + start_value + '*.' + end_value)  # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    print("Using glob: ")
+    print(latest_file)
+
+verify_jaser_report_file_download('BatchList', 'pdf')
+get_only_latest_file('BatchList', 'pdf')
+using_glob('BatchList', 'pdf')
+
