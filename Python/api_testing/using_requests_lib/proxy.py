f3a186d1c12643987bad208149953fd5baffc251
Anish Sebastian <anishst@hotmail.com>
2020-07-10 14:44:43 -0400

    update

--- Python/api_testing/using_requests_lib/proxy.py
+++ Python/api_testing/using_requests_lib/proxy.py
@@ -1,7 +1,7 @@
 import requests
 
 # get an ip from here: https://free-proxy-list.net/
-proxies = { 'https://': '198.211.101.99:3128'}
+proxies = { 'https': '34.91.135.38:80'}
 
 # without proxy
 r = requests.get('https://httpbin.org/ip', verify=False)
