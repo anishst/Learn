7df316643e4de8b689c549be29a3cbe046b95b11
Anish Sebastian <anishst@hotmail.com>
2020-11-20 12:45:14 -0500

    layout practice with node js express

--- Programming/JavaScript/NodeJS/ToDoApp/app.js
+++ Programming/JavaScript/NodeJS/ToDoApp/app.js
@@ -7,6 +7,7 @@ const port = 3000
 
 // array to store items
 let items = ["Buy Food"]
+let workItems = []
 
 // use EJS in express app
 app.set('view engine', 'ejs');
@@ -27,17 +28,34 @@ app.get('/', (req, res) => {
 
     var day = new Date().toLocaleString("en-US", options)
     //  send value to list template file
-    res.render("list", { kindOfDay: day, newListItems: items })
+    res.render("list", { listTitle: day, newListItems: items })
 })
 
 app.post("/", (req, res) => {
 
     console.log(req.body.newItem)
-    //  add to array
-    items.push(req.body.newItem)
-    res.redirect("/");
+    let item = req.body.newItem
+
+    // check for listTitle value from list.ejs file and write to correct array
+    if (req.body.list === "Work") {
+        workItems.push(item)
+        res.redirect("/work")
+    } else {
+        items.push(item)
+        res.redirect("/");
+    }
+
 
 })
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work", newListItems: workItems })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
